dadd84fb7817d488348ab68bb791b299acd7807a
Om Duggineni <omduggineni@gmail.com>
2022-07-19 12:32:27 -0400

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['src/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./src/**/*.html"
+  ]
 }
