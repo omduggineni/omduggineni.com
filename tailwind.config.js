b67a47a23184d4ff3b1708942d8b45653a7915f3
Om Duggineni <omduggineni@gmail.com>
2022-07-22 18:10:20 -0400

    updated tailwind config to add screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,17 @@
 module.exports = {
   content: [
     "./src/**/*.html"
-  ]
+  ],
+  screens: {
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px',
+    'mobile': '320px',
+    'tablet': '768px',
+    'desktop': '1024px',
+    'desktop-lg': '1280px',
+    'desktop-xl': '1536px',
+  }
 }
