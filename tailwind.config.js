3387ed26219bb99d55c17be2b2de186af5942d34
Om Duggineni <omduggineni@gmail.com>
2022-07-22 21:19:49 -0400

    added media queries for some responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,13 @@ module.exports = {
   content: [
     "./src/**/*.html"
   ],
-  screens: {
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
-    '2xl': '1536px',
-    'mobile': '320px',
-    'tablet': '768px',
-    'desktop': '1024px',
-    'desktop-lg': '1280px',
-    'desktop-xl': '1536px',
+  theme: {
+    screens: {
+      'mobile': '320px',
+      'desktop-sm': '768px',
+      'desktop': '1024px',
+      'desktop-lg': '1280px',
+      'desktop-xl': '1536px',
+    }
   }
 }
