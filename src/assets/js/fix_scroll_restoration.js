9c90ff0047799485732ded0c6e65c8e335ae08e7
Om Duggineni <omduggineni@gmail.com>
2022-07-24 12:38:42 -0400

    updated fonts and added scroll restoration

--- src/assets/js/fix_scroll_restoration.js
+++ src/assets/js/fix_scroll_restoration.js
@@ -1,3 +1,25 @@
 if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual'; //bypass scroll restoration, which doesn't work properly with Chrome
+    history.scrollRestoration = 'manual'; //bypass scroll restoration, which Chrome does wrong due to the scrollpill
 }
+
+const serializePathname = function(pathname) {
+    return pathname.replace(/\//g, '_');
+}
+
+// on page hide, save scroll position along with current URL
+window.addEventListener('pagehide', function (){
+    const pathname = window.location.pathname;
+    const serializedPathname = serializePathname(pathname);
+    const scrollPosition = window.scrollY;
+    sessionStorage.setItem(serializedPathname+"_savedScrollPosition", scrollPosition);
+});
+
+//if scroll position is saved, restore it
+window.addEventListener('pageshow', function (){
+    const pathname = window.location.pathname;
+    const serializedPathname = serializePathname(pathname);
+    const scrollPosition = sessionStorage.getItem(serializedPathname+"_savedScrollPosition");
+    if (scrollPosition) {
+        window.scrollTo(0, scrollPosition);
+    }
+});
