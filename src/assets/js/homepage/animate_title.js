6caae7afa1c57ae86defc576137619039317cdaa
Om Duggineni <omduggineni@gmail.com>
2022-11-18 08:59:48 -0500

    updated title animation

--- src/assets/js/homepage/animate_title.js
+++ src/assets/js/homepage/animate_title.js
@@ -1,72 +1,73 @@
-const titleAnimationVersion=2;
+const titleAnimationVersion = 3;
 
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 var TitleElement = null;
 var TitleAnimationTexts = ["science nerd ", "hardworking academic ", "amateur photographer ", "Om Duggineni "];
 
-var titleAnimationHideEverything = function(){
+var titleAnimationHideEverything = function () {
     let elements_on_page = document.body.querySelectorAll(":not(.necessary-for-title-animation)");
     //console.log(elements_on_page);
-    elements_on_page.forEach(function(element){
+    elements_on_page.forEach(function (element) {
         element.classList.add("not_visible");
     });
 };
 
-var titleAnimationBringIntoView = function(){
+var titleAnimationBringIntoView = function () {
     let elements_on_page = document.body.querySelectorAll(":not(.necessary-for-title-animation)");
-    elements_on_page.forEach(function(element){
+    elements_on_page.forEach(function (element) {
         element.classList.add("bring_into_view");
         element.classList.remove("not_visible");
     });
-    setTimeout(()=>{
-        elements_on_page.forEach(function(element){
+    setTimeout(() => {
+        elements_on_page.forEach(function (element) {
             element.classList.remove("bring_into_view");
         });
     }, 1000);
 };
 
-const titleAnimationTypewriteTitle = async(title_elements, typing_indicator_element, text, delay, untypewrite) =>{
+const titleAnimationTypewriteTitle = async (title_elements, typing_indicator_element, text, delay, untypewrite) => {
     //TitleElement
     let delays = [];
-    for(let i = 0; i < text.length; i++){
+    for (let i = 0; i < text.length; i++) {
         title_elements[i].innerText = text[i];
         typing_indicator_element.style.opacity = "1";
-        let current_delay = Math.random()*delay+delay;
+        let current_delay = Math.random() * delay + delay;
         delays.push(current_delay);
         await sleep(current_delay);
     }
-    if(untypewrite){
+    if (untypewrite) {
         //untypewrite
-        for(let i = text.length-1; i >= 0; i--){
+        for (let i = text.length - 1; i >= 0; i--) {
             title_elements[i].innerText = "";
             typing_indicator_element.style.opacity = "1";
-            let current_delay = delays[i]*0.9;
+            let current_delay = delays[i] * 0.9;
             await sleep(current_delay);
         }
     }
 }
-const titleAnimationToggleTypingIndicatorHandler = function(typing_indicator_element){
-    return ()=>{
-        if(typing_indicator_element.style.opacity === "1"){
+const titleAnimationToggleTypingIndicatorHandler = function (typing_indicator_element) {
+    return () => {
+        if (typing_indicator_element.style.opacity === "1") {
             typing_indicator_element.style.opacity = "0";
-        }else{
+        } else {
             typing_indicator_element.style.opacity = "1";
         }
     }
 }
-const executeTitleAnimation = async() => {
+const executeTitleAnimation = async () => {
+    titleAnimationHideEverything();
     let promise = new Promise((resolve) => {
-        window.addEventListener('DOMContentLoaded', async()=>{
+        window.addEventListener('DOMContentLoaded', async () => {
             let TitleElement = document.getElementById("page-title");
             TitleElement.innerText = "";
 
             let TitleTextElements = [];
             let max_TitleTextElements = 0;
-            for(let i = 0; i < TitleAnimationTexts.length; i++){
+            for (let i = 0; i < TitleAnimationTexts.length; i++) {
                 max_TitleTextElements = Math.max(max_TitleTextElements, TitleAnimationTexts[i].length);
             }
-            for(let i = 0; i < max_TitleTextElements; i++){
+            for (let i = 0; i < max_TitleTextElements; i++) {
                 let TitleTextElement = document.createElement("span");
                 TitleTextElement.classList.add("necessary-for-title-animation");
                 TitleTextElements.push(TitleTextElement);
@@ -79,21 +80,16 @@ const executeTitleAnimation = async() => {
             TitleElement.appendChild(TitleTypingElement);
             setInterval(titleAnimationToggleTypingIndicatorHandler(TitleTypingElement), 1000);
 
-            if(localStorage.getItem("title-animation-done") !== "true" && !(localStorage.getItem("title-animation-version") || false) && parseInt(localStorage.getItem("title-animation-version")) !== titleAnimationVersion){
-                titleAnimationHideEverything();
-                for(let i = 0; i < TitleAnimationTexts.length-1; i++){
-                    await titleAnimationTypewriteTitle(TitleTextElements, TitleTypingElement, TitleAnimationTexts[i], 70, true);
-                    await sleep(400);
-                }
-                await titleAnimationTypewriteTitle(TitleTextElements, TitleTypingElement, TitleAnimationTexts[TitleAnimationTexts.length-1], 70, false);
+            if (localStorage.getItem("title-animation-done") !== "true" && !(localStorage.getItem("title-animation-version") || false) && parseInt(localStorage.getItem("title-animation-version")) !== titleAnimationVersion) {
+                await titleAnimationTypewriteTitle(TitleTextElements, TitleTypingElement, TitleAnimationTexts[TitleAnimationTexts.length - 1], 70, false);
                 titleAnimationBringIntoView();
                 localStorage.setItem("title-animation-done", "true");
-                localStorage.setItem("title-animation-version", titleAnimationVersion+"");
+                localStorage.setItem("title-animation-version", titleAnimationVersion + "");
             }
 
             resolve();
             let elements_necessary_for_title_animation = document.body.querySelectorAll(".necessary-for-title-animation");
-            elements_necessary_for_title_animation.forEach(function(element){
+            elements_necessary_for_title_animation.forEach(function (element) {
                 element.classList.remove("necessary-for-title-animation");
             });
 
@@ -109,9 +105,9 @@ const executeTitleAnimation = async() => {
             //     TitleElement.appendChild(TitleTextElement);
             // }
 
-            await titleAnimationTypewriteTitle(TitleTextElements, TitleTypingElement, TitleAnimationTexts[TitleAnimationTexts.length-1], 70, false);
+            await titleAnimationTypewriteTitle(TitleTextElements, TitleTypingElement, TitleAnimationTexts[TitleAnimationTexts.length - 1], 70, false);
             await sleep(1000);
-            
+
         });
     });
     return promise;
