6406d2014b9edd08d22b22ab0f282f3498b44ba3
Om Duggineni <omduggineni@gmail.com>
2023-02-23 11:41:32 -0500

    rewrote all css in scss

--- src/assets/js/particle_system.js
+++ src/assets/js/particle_system.js
@@ -2,27 +2,27 @@ const pix2browserstr = (pix) => {
     return pix + 'px';
 }
 
-class ValueNoiseGenerator{
-    constructor(max_value, max_steps){
+class ValueNoiseGenerator {
+    constructor(max_value, max_steps) {
         this.value = Math.random() * max_value;
         this.next_value = Math.random() * max_value;
-        this.next_value_steps = Math.floor((Math.sqrt(Math.random()) * (max_steps-1)) + 1);
+        this.next_value_steps = Math.floor((Math.sqrt(Math.random()) * (max_steps - 1)) + 1);
         this.max_value = max_value;
         this.max_steps = max_steps;
         this.step_num = 0;
     }
-    ease(a, b, t){
+    ease(a, b, t) {
         return a + (b - a) * t; //linear interp for now
     }
-    getValue(){
+    getValue() {
         let value = this.ease(this.value, this.next_value, this.step_num / this.next_value_steps);
         //console.log(this.value, this.next_value, this.step_num, this.next_value_steps, value);
         this.step_num++;
-        if(this.step_num == this.next_value_steps){
+        if (this.step_num == this.next_value_steps) {
             this.step_num = 0;
             this.value = this.next_value;
             this.next_value = Math.random() * this.max_value;
-            this.next_value_steps = Math.floor((Math.sqrt(Math.random()) * (this.max_steps-1)) + 1);
+            this.next_value_steps = Math.floor((Math.sqrt(Math.random()) * (this.max_steps - 1)) + 1);
         }
         return value;
     }
@@ -35,25 +35,25 @@ const particleSay = (text, parentElement, x, y, lifetime) => {
     parentElement.appendChild(element);
     element.style.top = y + 'px';
     element.style.left = x + 'px';
-    element.style.animationDuration = lifetime/1000 + 's';
+    element.style.animationDuration = lifetime / 1000 + 's';
     setTimeout(element.remove.bind(element), lifetime);
     return element;
 }
 const particlesSay = (text, particles, lifetime) => {
     let elements = [];
     let parentElement = particles[0]._parentSystem.text_element;
-    for(let i = 0; i < particles.length; i++){
+    for (let i = 0; i < particles.length; i++) {
         let element = document.createElement('span');
         element.classList.add('particle-system-text');
         element.style.top = particles[i]._y + 'px';
         element.style.left = particles[i]._x + 'px';
-        element.style.animationDuration = lifetime/1000 + 's';
+        element.style.animationDuration = lifetime / 1000 + 's';
         element.innerText = text;
         elements.push(element);
         parentElement.appendChild(element);
     }
     setTimeout(() => {
-        for(let i = 0; i < elements.length; i++){
+        for (let i = 0; i < elements.length; i++) {
             elements[i].remove();
         }
     }, lifetime);
@@ -61,98 +61,98 @@ const particlesSay = (text, particles, lifetime) => {
 }
 
 
-class UtterancesObject{
-    constructor(utterances){
+class UtterancesObject {
+    constructor(utterances) {
         this.particles = [];
         this.utterances = utterances;
         this.utterance_index = 0
         this.is_speaking = false;
     }
-    add_particle(particle){
+    add_particle(particle) {
         this.particles.push(particle);
     }
-    make_utterance(){
-        if(this.particles.length == 0){
+    make_utterance() {
+        if (this.particles.length == 0) {
             this.is_speaking = false;
             return false;
         }
         let utterance = this.utterances[this.utterance_index];
-        particlesSay(utterance[0], this.particles, utterance[1]*1000);
+        particlesSay(utterance[0], this.particles, utterance[1] * 1000);
         this.utterance_index++;
         return this.utterance_index < this.utterances.length;
     }
-    speak(){
-        if(this.make_utterance()){
+    speak() {
+        if (this.make_utterance()) {
             this.is_speaking = true;
-            setTimeout(this.speak.bind(this), this.utterances[this.utterance_index-1][1]*1000);
-        }else{
+            setTimeout(this.speak.bind(this), this.utterances[this.utterance_index - 1][1] * 1000);
+        } else {
             this.is_speaking = false;
             this.utterance_index = 0;
         }
     }
-    speakIn(time){
-        if(!this.is_speaking){
-            if(this.speakTimeout || false) clearTimeout(this.speakTimeout);
+    speakIn(time) {
+        if (!this.is_speaking) {
+            if (this.speakTimeout || false) clearTimeout(this.speakTimeout);
             this.speakTimeout = setTimeout(this.speak.bind(this), time);
         }
     }
 }
 
-class Particle{
-    constructor(parentElement, parentSystem){
+class Particle {
+    constructor(parentElement, parentSystem) {
         this._speed = Particle.SPEED;
         this._parentSystem = parentSystem;
         this._x = Math.random() * this._parentSystem.width;
         this._y = Math.random() * this._parentSystem.height;
         this._z_generator = new ValueNoiseGenerator(10, 100);
-        this._z = this._z_generator.getValue()+10;
+        this._z = this._z_generator.getValue() + 10;
         //this.brightness = new ValueNoiseGenerator(1, 100);
         this._theta_generator = new ValueNoiseGenerator(Math.PI * 2, 500);
         this._theta = this._theta_generator.getValue();
-        
+
         this.element = document.createElementNS("http://www.w3.org/2000/svg", "circle");
         this.element.setAttribute("cx", this._x);
         this.element.setAttribute("cy", this._y);
-        this.element.setAttribute("r", this._z/2);
-        this.element.setAttribute("opacity", Math.min(2/(this._z), 1));
+        this.element.setAttribute("r", this._z / 2);
+        this.element.setAttribute("opacity", Math.min(2 / (this._z), 1));
         this.element.setAttribute("fill", "#ffff00");
         parentElement.appendChild(this.element);
     }
-    update(){
+    update() {
         //particle calculation logic
         this._x = (this._x + (Math.cos(this._theta) * this._speed) + this._parentSystem.width) % this._parentSystem.width;
         this._y = (this._y + (Math.sin(this._theta) * this._speed) + this._parentSystem.height) % this._parentSystem.height;
         this._theta = this._theta_generator.getValue();//Math.random() * 0.2 - 0.1;
         //console.log(this._theta);
         this._speed += (Math.random() - Math.random()) * 0.1;
-        this._speed = Particle.SPEED + (this._speed-Particle.SPEED) * (this.SPEED > Particle.SPEED ? 0.998 : 0.95);
-        this._z = this._z_generator.getValue()+10;
+        this._speed = Particle.SPEED + (this._speed - Particle.SPEED) * (this.SPEED > Particle.SPEED ? 0.998 : 0.95);
+        this._z = this._z_generator.getValue() + 10;
 
         //if(Math.random() > 0.999) particleSay("Hello!", this._parentSystem.text_element, this._x, this._y, 1000);
 
         //actual UI update
         this.element.setAttribute("cx", this._x);
         this.element.setAttribute("cy", this._y);
-        this.element.setAttribute("r", this._z/2);
-        this.element.setAttribute("opacity", Math.min(2/(this._z), 1));
+        this.element.setAttribute("r", this._z / 2);
+        this.element.setAttribute("opacity", Math.min(2 / (this._z), 1));
     }
-    sayUtterances(utterances, delay_start){
+    sayUtterances(utterances, delay_start) {
         //console.log(utterances);
         utterances.add_particle(this);
         utterances.speakIn(delay_start);
     }
-    raycast(origin_x, origin_y, direction_x, direction_y){
+    raycast(origin_x, origin_y, direction_x, direction_y) {
         //TODO: implement raycast
     }
 }
-Particle.SPEED=1;
-Particle.WIDTH=15;
-class ParticleSystem{
-    constructor(parentElement, parentTextElement, numParticles){
+Particle.SPEED = 1;
+Particle.WIDTH = 15;
+class ParticleSystem {
+    constructor(parentElement, parentTextElement, numParticles) {
         this._ticknum = 0;
 
-        this.width = window.innerWidth-Particle.WIDTH;
-        this.height = document.body.scrollHeight-Particle.WIDTH;
+        this.width = window.innerWidth - Particle.WIDTH;
+        this.height = document.body.scrollHeight - Particle.WIDTH;
 
         parentElement.style.height = this.height + 'px';
         parentTextElement.style.height = this.height + 'px';
@@ -161,35 +161,35 @@ class ParticleSystem{
         this.text_element = parentTextElement;
 
         this.particles = [];
-        let addParticle = ()=>{
+        let addParticle = () => {
             this.particles.push(new Particle(this.element, this));
         }
-        for(let i = 0; i < numParticles; i++){
+        for (let i = 0; i < numParticles; i++) {
             setTimeout(addParticle, i);
         }
         this.update_this = this.update.bind(this);
         requestAnimationFrame(this.update_this);
         window.addEventListener('resize', this.on_resize.bind(this));
     }
-    on_resize(){
+    on_resize() {
         this.width = window.innerWidth;
         this.height = document.body.scrollHeight;
         this.element.style.height = this.height + 'px';
         this.text_element.style.height = this.height + 'px';
     }
-    update(){
-        for(let i = 0; i < this.particles.length; i++){
+    update() {
+        for (let i = 0; i < this.particles.length; i++) {
             this.particles[i].update();
         }
         this._ticknum++;
         requestAnimationFrame(this.update_this);
     }
-    raycast(origin_x, origin_y, direction_x, direction_y){
+    raycast(origin_x, origin_y, direction_x, direction_y) {
         let closest_t = -1;
         let closest = null;
-        for(let i = 0; i < this.particles.length; i++){
+        for (let i = 0; i < this.particles.length; i++) {
             let t = this.particles[i].raycast(origin_x, origin_y, direction_x, direction_y);
-            if(t > 0 && (closest_t < 0 || t < closest_t)){
+            if (t > 0 && (closest_t < 0 || t < closest_t)) {
                 closest_t = t;
                 closest = this.particles[i];
             }
@@ -203,4 +203,4 @@ class ParticleSystem{
 // };
 // export {ParticleSystem, Particle, UtterancesLibrary};
 
-export {ParticleSystem, Particle};
+export { ParticleSystem, Particle };
