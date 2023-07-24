64814f3ba7d5df85247cbe501385f00684da966b
Om Duggineni <omduggineni@gmail.com>
2023-07-23 20:14:42 -0400

    overhauled internal scrollpill workings

--- src/assets/js/scrollpill.js
+++ src/assets/js/scrollpill.js
@@ -1,138 +1,141 @@
-const fontawesome_icons = {
-    BARS: "&#xf0c9;",
-    ARROW_UP: "&#xf062;"
-};
-
-class ScrollPillNav {
+class ScrollPillNavbar {
     constructor(element) {
         this.element = element;
-        this.is_hovered = false;
-        this.element.addEventListener('mouseenter', this.handle_hoverstart.bind(this));
-        this.element.addEventListener('mouseleave', this.handle_hoverend.bind(this));
+        element.style.display = 'block';
+        this.is_open = true;
     }
-    handle_hoverstart(){
-        this.is_hovered = true;
-        this.element.classList.add("scroll-pill-nav-hovered");
-    }
-    handle_hoverend(){
-        this.is_hovered = false;
-        this.element.classList.remove("scroll-pill-nav-hovered");
-    }
-    show(){
+    show() {
+        if(this.is_open) return;
         this.element.style.display = 'block';
+        this.is_open = true;
     }
-    hide(){
+    hide() {
+        if(!this.is_open) return;
         this.element.style.display = 'none';
+        this.is_open = false;
+    }
+    toggle() {
+        if(this.is_open) this.hide();
+        else this.show();
+    }
+    disable() {
+        this.oldShow = this.show;
+        this.oldHide = this.hide;
+        this.show = () => { };
+        this.hide = () => { };
+    }
+    enable() {
+        this.show = this.oldShow;
+        this.hide = this.oldHide;
+    }
+}
+
+class ScrollPillElement {
+    state_machine = {
+        AT_TOP_FIRST: 'scrollpill-at-top-first',
+        AT_TOP: 'scrollpill-at-top',
+        AT_MIDDLE: 'scrollpill-at-middle',
+        AT_BOTTOM: 'scrollpill-at-bottom',
+    };
+    drag_states = {
+        DRAGGING: 'scrollpill-dragging',
+    };
+    constructor(element) {
+        this.element = element;
+        this.element_height = element.getBoundingClientRect().height;
+        this.navbar = new ScrollPillNavbar(element.querySelector('.scrollpill-navbar'));
+        element.appendChild(this.navbar.element);
+        element.classList.add('scrollpill');
+        element.classList.add('scrollpill-at-top-first');
+        this.state = this.state_machine.AT_TOP_FIRST;
+        this.scrollPercentage = 0;
+
+        this.registerEventHandlers();
+    }
+    registerEventHandlers() {
+        window.addEventListener('scroll', this.onScrollUpdate.bind(this));
+        window.addEventListener('resize', this.onScrollUpdate.bind(this));
+        this.element.addEventListener('click', this.onClick.bind(this));
+    }
+    updateState(state) {
+        this.element.classList.remove(this.state);
+        this.element.classList.add(state);
+        this.state = state;
+    }
+    percentage2screenpx(percentage) {
+        //console.log(percentage);
+        let remaining_screen_space = window.innerHeight - this.element_height * 1.775;
+        
+        return ((percentage/100) * remaining_screen_space) + 'px';
+    }
+    percentage2windowpx(percentage) {
+        return (percentage / 100) * (document.body.scrollHeight - window.innerHeight);
+    }
+    onScrollUpdate(event) {
+        const scrollY = window.scrollY;
+        const scrollHeight = document.body.scrollHeight;
+        const windowHeight = window.innerHeight;
+
+        const scrollPercentage = (scrollY / (scrollHeight - windowHeight)) * 100;
+        
+        if (this.element.classList.contains('scrollpill-open')) {
+            this.element.classList.remove('scrollpill-open');
+        }
+
+        this.scrollPercentage = scrollPercentage;
+        this.element.style.top = this.percentage2screenpx(scrollPercentage);
+        if(scrollPercentage == 0){
+            this.updateState(this.state_machine.AT_TOP);
+            this.navbar.show();
+        } else if (scrollPercentage >= 99.75) {
+            this.updateState(this.state_machine.AT_BOTTOM);
+            this.navbar.hide();
+        } else {
+            this.updateState(this.state_machine.AT_MIDDLE);
+            this.navbar.hide();
+        }
+    }
+    onPercentageUpdate(percentage, {scrollBehavior = 'instant'} = {}) {
+        // scroll the window to the correct position
+        window.scrollTo({
+            right: 0, top: this.percentage2windowpx(percentage), behavior: scrollBehavior
+        });
+        this.onScrollUpdate();
+    }
+    onClick() {
+        if (this.state == this.state_machine.AT_BOTTOM) {
+            this.navbar.disable();
+            this.onPercentageUpdate(0, { scrollBehavior: 'smooth' });
+            let scrollHandler = () => {
+                if (this.scrollPercentage == 0) {
+                    window.removeEventListener('scroll', scrollHandler);
+                    this.navbar.enable();
+                    this.navbar.show();
+                }
+            };
+            window.addEventListener('scroll', scrollHandler);
+            setTimeout(() => {
+                window.removeEventListener('scroll', scrollHandler);
+                this.navbar.enable();
+                this.navbar.show();
+            }, 2000);
+        }
+
+        if (this.navbar.is_open) {
+            this.element.classList.remove("scrollpill-open");
+            this.navbar.hide();
+        } else {
+            this.element.classList.add("scrollpill-open");
+            this.navbar.show();
+        }
     }
 }
 
 class ScrollPill{
-    constructor(element_id, options){
-        this.element = document.getElementById(element_id);
-        this.element.classList.add('scrollpill');
-        this.element.classList.add('scrollpill-at-top-first');
-        this.burger_element = document.getElementById("scrollpill-burger_element");
-        this.scroll_percent = 0;
-        this.is_hovered = false;
-        this.options = options;
-        this.navbar = new ScrollPillNav(this.element.querySelector('.scrollpill-navbar'));
-        this.navbar.show();
-        this.navbar.is_open = false;
-        this.element.appendChild(this.navbar.element);
-
-        //initialization of handlers
-        this.burger_element.innerHTML = fontawesome_icons.BARS;
-        this.handler_scroll = this.handleScroll.bind(this);
-        window.addEventListener('scroll', this.handler_scroll);
-        this.handler_mouseover = this.onmouseover.bind(this);
-        this.element.addEventListener('mouseenter', this.handler_mouseover);
-        this.handler_mouseoff = this.onmouseoff.bind(this);
-        this.element.addEventListener('mouseleave', this.handler_mouseoff);
-        this.handler_onclick = this.onclick.bind(this);
-        this.element.addEventListener('mousedown', this.handler_onclick);
-    }
-    handleScroll(event){
-        const scrollY = window.pageYOffset;
-        const scrollHeight = document.body.scrollHeight;
-        const windowHeight = window.innerHeight;
-        const scrolled = (scrollY / (scrollHeight - windowHeight)) * 100;
-        this.scroll_percent = scrolled;
-        this.element.classList.remove('scrollpill-open');
-        if(scrolled === 0){
-            this.element.classList.add('scrollpill-at-top');
-            this.element.classList.remove('scrollpill-at-bottom');
-            this.element.classList.remove('scrollpill-at-middle');
-            this.element.classList.remove('scrollpill-at-top-first');
-            setTimeout(()=>{
-                this.burger_element.innerHTML = fontawesome_icons.BARS;
-                this.navbar.show();
-            }, 200);
-        }else if(scrolled >= 100){
-            this.element.classList.add('scrollpill-at-bottom');
-            this.element.classList.remove('scrollpill-at-top');
-            this.element.classList.remove('scrollpill-at-middle');
-            this.element.classList.remove('scrollpill-at-top-first');
-            if(!this.is_hovered){
-                this.navbar.hide();
-            }
-            setTimeout(()=>{
-                this.burger_element.innerHTML = fontawesome_icons.ARROW_UP;
-            }, 200);
-        }else{
-            this.element.classList.remove('scrollpill-at-top');
-            this.element.classList.remove('scrollpill-at-bottom');
-            this.element.classList.add('scrollpill-at-middle');
-            this.element.classList.remove('scrollpill-at-top-first');
-            if(!this.is_hovered){
-                this.burger_element.innerHTML = "";
-                this.navbar.is_open = false;
-                this.navbar.hide();
-            }
-        }
-        this.element.style.top = (scrolled*(windowHeight - 90)/(windowHeight)) + '%';
-    }
-    onmouseover(event){
-        this.is_hovered = true;
-        if(this.scroll_percent != 0 && this.scroll_percent != 100){
-            setTimeout(()=>{this.burger_element.innerHTML = fontawesome_icons.BARS;}, 200);
-        }
-    }
-    onmouseoff(event){
-        this.is_hovered = false;
-        if(this.scroll_percent != 0 && this.scroll_percent != 100 && !this.navbar.is_open){
-            this.burger_element.innerHTML = "";
-            setTimeout(()=>{this.burger_element.innerHTML = "";}, 200);
-        }
-    }
-    onclick(event){
-        if(this.scroll_percent >= 100){
-            window.location.replace("#top");
-            this.update(0);
-            //this.element.classList.add('scrollpill-at-top-first');
-            this.burger_element.innerHTML = "";
-        }else{
-            if(this.navbar.is_open){
-                if(this.scroll_percent == 0){
-                    this.navbar.is_open = false;
-                }else{
-                    this.element.classList.remove('scrollpill-open')
-                    if(!this.navbar.is_hovered){
-                        this.navbar.hide()
-                    }
-                }
-            }else{
-                this.navbar.show();
-                if(this.scroll_percent > 0){
-                    this.element.classList.add('scrollpill-open')
-                }
-                setTimeout(()=>{this.burger_element.innerHTML = fontawesome_icons.BARS;}, 200);
-            }
-            this.navbar.is_open = !this.navbar.is_open;
-        }
-    }
-    update(percent){
-        window.scrollTo(0, percent / 100 * (document.body.scrollHeight - window.innerHeight));
-        this.handleScroll(null);
+    constructor(element_id, options) {
+        let element = document.getElementById(element_id);
+        this.element = new ScrollPillElement(element);
+        this.options = options ?? {};
     }
 }
 
